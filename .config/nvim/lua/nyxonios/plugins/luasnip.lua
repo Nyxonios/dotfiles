43ee0163524ce57c6d61ef6d7e10318cd18392cf
martin.seller@schibsted.com <martin.seller@schibsted.com>
2024-12-17 10:46:02 +0100

    chore(nvim): upgraded lua snippet keybinds

--- .config/nvim/lua/nyxonios/plugins/luasnip.lua
+++ .config/nvim/lua/nyxonios/plugins/luasnip.lua
@@ -8,6 +8,21 @@ return {
   },
   config = function()
     vim.keymap.set('i', '<c-u>', require 'luasnip.extras.select_choice')
+    vim.keymap.set({ 'i' }, '<C-K>', function()
+      ls.expand()
+    end, { silent = true })
+    vim.keymap.set({ 'i', 's' }, '<C-L>', function()
+      ls.jump(1)
+    end, { silent = true })
+    vim.keymap.set({ 'i', 's' }, '<C-J>', function()
+      ls.jump(-1)
+    end, { silent = true })
+
+    vim.keymap.set({ 'i', 's' }, '<C-E>', function()
+      if ls.choice_active() then
+        ls.change_choice(1)
+      end
+    end, { silent = true })
     require('luasnip.loaders.from_lua').lazy_load { paths = { vim.fn.stdpath 'config' .. '/lua/nyxonios/luasnippets' } }
   end,
 }
