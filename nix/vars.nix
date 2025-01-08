f9545ad4cfa8c41db9d340cf13d411d8766dc210
Martin Seller <mseller@evroc.com>
2025-01-08 07:33:10 +0100

    fix: vars should be empty in repo

--- nix/vars.nix
+++ nix/vars.nix
@@ -1,10 +1,10 @@
 { pkgs, ... }:
 {
   userData = rec {
-    user = "nyxonios";
+    user = "";
     homeDir = if pkgs.stdenv.isDarwin then "/Users/${user}" else "/home/${user}";
-    userEmail = "martin.n.seller@gmail.com";
-    platform = "x86_64-linux";
+    userEmail = "";
+    platform = "";
   };
 }
 
