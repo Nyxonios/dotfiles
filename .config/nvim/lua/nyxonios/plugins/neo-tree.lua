b722a376a0a322971f457e59cfa17cff11a45965
Martin Seller <mseller@evroc.com>
2025-01-10 09:26:39 +0000

    change(nvim): Follow files in NeoTree

--- .config/nvim/lua/nyxonios/plugins/neo-tree.lua
+++ .config/nvim/lua/nyxonios/plugins/neo-tree.lua
@@ -1,22 +1,27 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
   dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
   },
   config = function()
-    require('neo-tree').setup({
+    require('neo-tree').setup {
       close_if_last_window = true,
       filesystem = {
+        follow_current_file = {
+          enabled = true, -- This will find and focus the file in the active buffer every time
+          --               -- the current file is changed while the tree is open.
+          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+        },
         filtered_items = {
           visible = true,
           never_show = {
-            ".git"
-          }
+            '.git',
+          },
         },
       },
-    })
+    }
   end,
 }
