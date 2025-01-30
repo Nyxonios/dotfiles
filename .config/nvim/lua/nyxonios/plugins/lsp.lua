f0362d96a76d0971863e2e4fafac31051c824481
Martin Seller <martin.n.seller@gmail.com>
2025-01-30 07:58:51 +0100

    chore(nvim): Added pickle language to lsp

--- .config/nvim/lua/nyxonios/plugins/lsp.lua
+++ .config/nvim/lua/nyxonios/plugins/lsp.lua
@@ -46,6 +46,31 @@ return {
       },
     },
   },
+  -- Pickle language
+  {
+    'apple/pkl-neovim',
+    lazy = true,
+    ft = 'pkl',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter',
+        build = function(_)
+          vim.cmd 'TSUpdate'
+        end,
+      },
+      'L3MON4D3/LuaSnip',
+    },
+    build = function()
+      require('pkl-neovim.internal').init()
+
+      -- Set up syntax highlighting.
+      vim.cmd 'TSInstall! pkl'
+    end,
+    config = function()
+      -- Set up snippets.
+      require('luasnip.loaders.from_snipmate').lazy_load()
+    end,
+  },
   {
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
