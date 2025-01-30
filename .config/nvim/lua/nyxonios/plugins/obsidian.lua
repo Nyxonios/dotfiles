7c8366bb7fc011c5b3f59f57879eb1387ed32596
Martin Seller <martin.n.seller@gmail.com>
2025-01-30 07:57:34 +0100

    feat(nvim): Added obsidian plugin

---
+++ .config/nvim/lua/nyxonios/plugins/obsidian.lua
@@ -0,0 +1,49 @@
+return {
+  'epwalsh/obsidian.nvim',
+  version = 'v3.9.0', -- recommended, use latest release instead of latest commit
+  lazy = false,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter',
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'vault',
+        path = '~/note-vault',
+      },
+    },
+  },
+
+  ---@param title string|?
+  ---@return string
+  note_id_func = function(title)
+    print 'hello, world'
+    -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+    -- In this case a note with the title 'My new note' will be given an ID that looks
+    -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+    local suffix = ''
+    if title ~= nil then
+      -- If title is given, transform it into valid file name.
+      suffix = title:gsub(' ', '-'):gsub('[^A-Za-z0-9-]', ''):lower()
+    else
+      -- If title is nil, just add 4 random uppercase letters to the suffix.
+      for _ = 1, 4 do
+        suffix = suffix .. string.char(math.random(65, 90))
+      end
+    end
+    return tostring(os.time()) .. '-' .. suffix
+  end,
+}
