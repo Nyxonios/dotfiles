8c879eb2cabec29c697864bec06c3a1b75de7446
Martin Seller <martin.n.seller@gmail.com>
2025-08-22 07:07:54 +0200

    nvim(md): Set text preferences for md files

--- .config/nvim/lua/nyxonios/autocmd.lua
+++ .config/nvim/lua/nyxonios/autocmd.lua
@@ -12,3 +12,12 @@ vim.api.nvim_create_autocmd('FileType', {
   group = vim.api.nvim_create_augroup('set-tsc-compiler', { clear = true }),
   command = 'compiler tsc | setlocal makeprg=npx\\ tsc\\ --noEmit',
 })
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown,md',
+  callback = function()
+    local buf = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_option(buf, 'tabstop', 2)
+    vim.api.nvim_buf_set_option(buf, 'textwidth', 100)
+  end,
+})
