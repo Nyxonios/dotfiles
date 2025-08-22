184fdc5ae631f7a397fca10073f3759fef78f8ef
Martin Seller <martin.n.seller@gmail.com>
2025-08-22 07:09:19 +0200

    nvim(pkgs): Add render markdown plugin

--- .config/nvim/lua/nyxonios/plugins/markdown.lua
+++ .config/nvim/lua/nyxonios/plugins/markdown.lua
@@ -1,5 +1,15 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    opts = {},
+    config = function()
+      -- We disable by default.
+      require('render-markdown').disable()
+    end,
+  },
+  {
+    'richardbizik/nvim-toc',
+    opts = {},
+  },
 }
