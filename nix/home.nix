50ebdcfb767f4c8c560d616da54dc3afe8c4aded
Martin Seller <martin.n.seller@gmail.com>
2025-10-02 14:31:42 +0200

    cfg(nix): Fix after merge

--- nix/home.nix
+++ nix/home.nix
@@ -9,17 +9,7 @@ in
 {
   home.username = userData.user;
   home.homeDirectory = userData.homeDir;
-  nixpkgs.config.allowUnfree = true;
   xdg.enable = true;
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.05"; # Please read the comment before changing.
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
