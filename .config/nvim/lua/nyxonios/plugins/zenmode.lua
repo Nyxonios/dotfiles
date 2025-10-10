a93f6d2d4f7e886fecb04ceb99598d5668ad0be4
Martin Seller <martin.n.seller@gmail.com>
2025-10-10 15:07:21 +0200

    nvim(zenmode): Configure zenmore

--- .config/nvim/lua/nyxonios/plugins/zenmode.lua
+++ .config/nvim/lua/nyxonios/plugins/zenmode.lua
@@ -1,6 +1,10 @@
 return {
   'folke/zen-mode.nvim',
   opts = {
+    window = {
+      width = 150, -- width of the Zen window
+      height = 1, -- height of the Zen window
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
