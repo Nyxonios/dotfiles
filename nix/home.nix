fa4e67bb9f5652a968bd5e2b107f3b7f832a0114
Martin Seller <martin.n.seller@gmail.com>
2025-10-14 20:08:31 +0200

    nix(pkgs): Add obsidian

--- nix/home.nix
+++ nix/home.nix
@@ -69,6 +69,9 @@ in
     pkgs.awscli2
     pkgs.s3cmd
 
+    # Tools
+    pkgs.obsidian
+
     # Shell scripts
     (import ./scripts/tmux-sessionizer.nix { inherit pkgs; })
     (import ./scripts/notes.nix { inherit pkgs; })
