bed50b4b87f1ae49e03b987367b38876f63e54c3
Martin Seller <martin.n.seller@gmail.com>
2025-12-04 08:58:47 +0100

    nvim(neo-tree): Use libuv file watcher

--- .config/nvim/lua/nyxonios/plugins/neo-tree.lua
+++ .config/nvim/lua/nyxonios/plugins/neo-tree.lua
@@ -9,11 +9,12 @@ return {
   config = function()
     require('neo-tree').setup {
       close_if_last_window = true,
+      use_libuv_file_watcher = true,
       filesystem = {
         follow_current_file = {
-          enabled = true, -- This will find and focus the file in the active buffer every time
+          enabled = false, -- This will find and focus the file in the active buffer every time
           --               -- the current file is changed while the tree is open.
-          leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+          leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
         },
         filtered_items = {
           visible = true,
