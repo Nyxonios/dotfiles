25966b06ce9de4c349612e3e31c52224803b56ed
Martin Seller <martin.n.seller@gmail.com>
2026-02-05 10:49:34 +0100

    nvim: Add vim gloabl to lua LSP

--- .config/nvim/lua/nyxonios/plugins/lsp.lua
+++ .config/nvim/lua/nyxonios/plugins/lsp.lua
@@ -139,6 +139,9 @@ return {
         completion = {
           callSnippet = 'Replace',
         },
+        diagnostics = {
+          globals = { 'vim' },
+        },
         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
         -- diagnostics = { disable = { 'missing-fields' } },
       })
