a0c1d05081fd6ea0e0481be617aba37f00fa9c91
Martin Seller <martin.n.seller@gmail.com>
2026-04-24 07:38:10 +0200

    nix: Use Nix opencode for NixOS machines

--- nix/platforms/home-manager/_mixins/development/opencode.nix
+++ nix/platforms/home-manager/_mixins/development/opencode.nix
@@ -1,13 +1,14 @@
 # Opencode Configuration
+# Only enabled on NixOS machines
 
-{ config, pkgs, lib, host, ... }:
+{ config, pkgs, lib, host, customLib, ... }:
 
 let
   inherit (config.lib.file) mkOutOfStoreSymlink;
 in
 {
   config = {
-    # home.packages = [ pkgs.opencode ];
+    home.packages = customLib.mkIfPlatform "nixos" [ pkgs.opencode ] host;
     xdg.configFile.opencode.source = mkOutOfStoreSymlink "${host.home}/dotfiles/.config/opencode";
   };
 }
