d975894b9ed42a08c11bafa8849898ce546eec81
Martin Seller <martin.n.seller@gmail.com>
2026-06-02 18:54:07 +0200

    nvim: Add codelldb adapter and Zig debug configurations
    Add codelldb DAP adapter and Zig debug launch configurations to Neovim, enabling debugging of Zig projects with automatic pre-build steps and custom argument support. Also update the Karabiner external keyboard device address.

--- .config/nvim/lua/nyxonios/plugins/debug.lua
+++ .config/nvim/lua/nyxonios/plugins/debug.lua
@@ -23,6 +23,74 @@ return {
 
     require('dapui').setup()
 
+    -- CODELLB ADAPTER for Zig (and C/C++/Rust)
+    -- Install codelldb via one of:
+    --   macOS: brew install llvm (includes lldb, codelldb usually via extension)
+    --   Alternatively download VSCode codelldb extension and point to the binary
+    --   https://github.com/vadimcn/codelldb/releases
+    dap.adapters.codelldb = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        -- Adjust this path to your codelldb location:
+        command = vim.fn.exepath 'codelldb' or '/usr/local/bin/codelldb',
+        args = { '--port', '${port}' },
+      },
+    }
+
+    -- ZIG CONFIGURATION
+    dap.configurations.zig = {
+      {
+        name = 'Debug current Zig project',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          -- Build first, then debug
+          vim.fn.system 'zig build'
+          -- Try to find the binary in standard zig output paths
+          local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
+          local candidates = {
+            'zig-out/bin/' .. project_name,
+            'zig-out/bin/main',
+            'zig-cache/bin/' .. project_name,
+            'zig-cache/bin/main',
+          }
+          for _, path in ipairs(candidates) do
+            if vim.fn.filereadable(path) == 1 then
+              return vim.fn.getcwd() .. '/' .. path
+            end
+          end
+          -- Fallback: ask user
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = {},
+        env = function()
+          local variables = {}
+          for k, v in pairs(vim.fn.environ()) do
+            table.insert(variables, string.format('%s=%s', k, v))
+          end
+          return variables
+        end,
+      },
+      {
+        name = 'Debug Zig with args',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+          vim.fn.system 'zig build'
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/zig-out/bin/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+        args = function()
+          local args_string = vim.fn.input('Arguments: ')
+          return vim.split(args_string, ' +')
+        end,
+      },
+    }
+
     dap.listeners.before.attach.dapui_config = function()
       dapui.open()
     end
@@ -49,5 +117,10 @@ return {
     vim.keymap.set('n', '<leader>si', dap.step_into, {})
     vim.keymap.set('n', '<leader>s', dap.step_over, {}) -- (s)tep over
     vim.keymap.set('n', '<leader>tu', dapui.toggle, {}) -- (t)oggle (u)i
+
+    -- Zig specific keymaps
+    vim.keymap.set('n', '<leader>dz', function()
+      dap.continue()
+    end, { desc = '(d)ebug (z)ig current project' })
   end,
 }
